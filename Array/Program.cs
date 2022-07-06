d0eda6e6da9bf376deba324d465c88369bb0c8a8
Julio Alvarez <julioalvarezrd@gmail.com>
2022-07-06 10:48:07 -0400

    While y Do While

--- Array/Program.cs
+++ Array/Program.cs
@@ -1,16 +1,14 @@
-﻿
+﻿//También se le conoce como Matriz, la cual almacena una colección
+//de datos del mismo tipo (int, float, double, String, etc.), con
+//ello se evita declarar una infinidad de variables, del mismo tipo.
 
-string[] amigos = new string[]
+string[] amigos = new string[]//Arreglo sin numero de indice
 {
     "julio",
     "pedro"
 };
 
-
-int[] edad = new int[5];
-
-edad[0] = 18;
-
-
+int[] edad = new int[5]; //Arreglo sin datos
+edad[0] = 18; //Asignando un valor al indice 0
 
 Console.WriteLine(edad[0]);
---
+++ While/Program.cs
@@ -0,0 +1,16 @@
+﻿
+int num = 0;
+while(num<5)
+{
+    Console.WriteLine(num);
+    num++;
+}
+
+int i = 0;
+
+do
+{
+    Console.WriteLine(i);
+    i++;
+}
+while (i > 5);
