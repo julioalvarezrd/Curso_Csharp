aa2827f7271cbf7b2802f2476381b4a36667527f
Julio Alvarez <julioalvarezrd@gmail.com>
2022-07-06 08:39:32 -0400

    Funciones

---
+++ Funciones/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Las funciones nos ayuan a reutilizar codigo
+*/
+
+Saluda();
+
+int suma = Suma(5, 20);
+Console.WriteLine(suma);
+
+Console.WriteLine(Mul(1));
+
+
+
+
+
+//Funcion que no recibe ni retorna
+static void Saluda()
+{
+    Console.WriteLine("Hola");
+}
+
+//Funcion que recibe y retorna
+static int Suma(int num1, int num2)
+{
+    return num1 + num2;
+}
+
+//Funcion con un valor predeterminado
+static int Mul(int num1, int num2 = 2)
+{
+    return num1 * num2;
+}
--- TiposDeDatos/Program.cs
+++ TiposDeDatos/Program.cs
@@ -1,3 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+C# es altamente tipado por lo que tenemos diferentes
+tipos de datos, aqui un ejemplo de los que podemos
+utilizar en nuestro dia a dia.
+*/
 
+
+//Número entero: int
+int num1 = 15;
+
+//Números decimales: double
+double num2 = 10.56;
+
+//Cadena de texto: string
+string nombre = "Julio";
+
+//Character: char
+char caracter = 'A';
+
+//Boolean: bool
+bool activo = true;
