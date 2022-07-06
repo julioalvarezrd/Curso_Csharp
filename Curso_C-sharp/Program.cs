850a5f7fadb6ad27559914d04a18c7ffb7ac22d0
Julio Alvarez <julioalvarezrd@gmail.com>
2022-07-05 23:07:42 -0400

    Variables

--- Curso_C-sharp/Program.cs
+++ Curso_C-sharp/Program.cs
@@ -1,3 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Pimero indicamos el tipo de dato
+seguido del nombre de la variable
+*/
 
+//Número entero: int
+int num1 = 15;
+
+//Número flotante: float
+float num2 = 10;
+
+//Cadena de texto: string
+string nombre = "Julio";
+
+//Character: char
+char caracter = 'A';
+
+//Boolean: bool
+bool activo = true;
+
+Console.WriteLine(nombre + num1);
